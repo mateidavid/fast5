c66426e81831fc45213487f1569575a6e1ad1cd1
Matei David <matei@cs.toronto.edu>
2017-02-24 17:12:24 -0500

    version: 0.6.0

--- src/fast5_version.hpp
+++ src/fast5_version.hpp
@@ -1,10 +1,3 @@
-//
-// Part of: https://github.com/mateidavid/fast5
-//
-// Copyright (c) 2015-2017 Matei David, Ontario Institute for Cancer Research
-// MIT License
-//
-
 #ifndef __FAST5_VERSION_HPP
 #define __FAST5_VERSION_HPP
 
@@ -14,7 +7,7 @@ namespace fast5
 namespace
 {
 
-static char const * const version = "0.5.9";
+static char const * const version = "0.6.0";
 
 }
 
